bfbef193bba6e32c4c8d21f1642f64a0ba0e4875
woodpicker-nick <nickkg001@gmail.com>
2026-02-13 14:04:14 +0400

    fix: 修复loading内存溢出问题

--- packages/vant/src/loading/Loading.tsx
+++ packages/vant/src/loading/Loading.tsx
@@ -10,16 +10,6 @@ import {
 
 const [name, bem] = createNamespace('loading');
 
-const SpinIcon: JSX.Element[] = Array(12)
-  .fill(null)
-  .map((_, index) => <i class={bem('line', String(index + 1))} />);
-
-const CircularIcon = (
-  <svg class={bem('circular')} viewBox="25 25 50 50">
-    <circle cx="50" cy="50" r="20" fill="none" />
-  </svg>
-);
-
 export type LoadingType = 'circular' | 'spinner';
 
 export const loadingProps = {
@@ -35,49 +25,42 @@ export type LoadingProps = ExtractPropTypes<typeof loadingProps>;
 
 export default defineComponent({
   name,
-
   props: loadingProps,
-
   setup(props, { slots }) {
     const spinnerStyle = computed(() =>
       extend({ color: props.color }, getSizeStyle(props.size)),
     );
 
-    const renderIcon = () => {
-      const DefaultIcon = props.type === 'spinner' ? SpinIcon : CircularIcon;
-      return (
-        <span class={bem('spinner', props.type)} style={spinnerStyle.value}>
-          {slots.icon ? slots.icon() : DefaultIcon}
-        </span>
+    const getIcon = () => {
+      return props.type === 'spinner' ? (
+        Array(12)
+          .fill(null)
+          .map((_, index) => <i class={bem('line', String(index + 1))} />)
+      ) : (
+        <svg class={bem('circular')} viewBox="25 25 50 50">
+          <circle cx="50" cy="50" r="20" fill="none" />
+        </svg>
       );
-    };
-
-    const renderText = () => {
-      if (slots.default) {
-        return (
-          <span
-            class={bem('text')}
-            style={{
-              fontSize: addUnit(props.textSize),
-              color: props.textColor ?? props.color,
-            }}
-          >
-            {slots.default()}
-          </span>
-        );
-      }
     };
 
     return () => {
       const { type, vertical } = props;
       return (
-        <div
-          class={bem([type, { vertical }])}
-          aria-live="polite"
-          aria-busy={true}
-        >
-          {renderIcon()}
-          {renderText()}
+        <div class={bem([type, { vertical }])}>
+          <span class={bem('spinner', props.type)} style={spinnerStyle.value}>
+            {slots.icon ? slots.icon() : getIcon()}
+          </span>
+          {slots.default ? (
+            <span
+              class={bem('text')}
+              style={{
+                fontSize: addUnit(props.textSize),
+                color: props.textColor ?? props.color,
+              }}
+            >
+              {slots.default()}
+            </span>
+          ) : null}
         </div>
       );
     };
